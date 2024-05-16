3d33e03e8066b0e255f011cfec8d689f453291fe
Augusto Guchea <aguchealezama@gmail.com>
2024-05-16 10:31:01 -0300

    nav

--- Front/react_kronos/src/index.js
+++ Front/react_kronos/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './components/button/buttons.jsx';
-import Table from './components/table/tables.jsx';
+import Barra from './components/navBar/navBars.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Button text="Button 1" numero={35} life={false}/>
-    <Button text="Button 1" life={true}/>
-    <Table/>
+    <Barra />
   </React.StrictMode>,
   document.getElementById('root')
 );
