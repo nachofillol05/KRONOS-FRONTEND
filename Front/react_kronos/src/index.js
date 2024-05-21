8a2e038e203befbc5b56f302ad5ab330988de238
nachofillol05 <nachofillol05@gmail.com>
2024-05-21 10:37:56 -0300

    agrego el select

--- Front/react_kronos/src/index.js
+++ Front/react_kronos/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/navBar/navBars.jsx';
 import TablaProfes from './components/App-Tabla-Profes/tabla-profes.jsx';
+import Select from './components/select/select.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar/>
-    <TablaProfes />
+    <TablaProfes/>
+    <Select name="TipoDocumento" style={{'--largo': `98px`}} solid/>
   </React.StrictMode>,
   document.getElementById('root')
 );
