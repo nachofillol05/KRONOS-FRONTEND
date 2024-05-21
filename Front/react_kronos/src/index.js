f4d148a01e3a9da53d8a3fb6e6fc8ba62930a3e8
JoaKox5 <joaquinparraga51@gmail.com>
2024-05-21 10:06:02 -0300

    update

--- Front/react_kronos/src/index.js
+++ Front/react_kronos/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './components/navBar/navBars.jsx';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
