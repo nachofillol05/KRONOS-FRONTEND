1d531683ccd13e0cdb9a7dcb8f03ef4dfb9a67b4
nachofillol05 <nachofillol05@gmail.com>
2024-05-21 10:37:28 -0300

    Agrego el select y tabla con su app

--- Front/react_kronos/src/components/table/tables.jsx
+++ Front/react_kronos/src/components/table/tables.jsx
@@ -1,61 +1,23 @@
 import {React, Component, useState, useEffect } from 'react';
 import './tables.scss';
-
-const data = [
-    { Nombre: "Anom", Documento: 1923244, Color: 30, Materia: "Matematica" },
-    { Nombre: "Megha", Documento: 19234234, Color: 30, Materia: "Biologia" },
-    { Nombre: "Subham", Documento: 2532432432, Color: 30, Materia: "Lengua" },
-]
-
-export default function Table(props) {
-    const [teachers, setTeachers] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/Kronosapp/teachers/', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                school_id: 1
-            })
-            }
-        )
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                setTeachers(data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-        
-        return(
-
-    <div class="grid-table">
-        <div class="header-container">
-            <div class="header">Nombre/s Apellido/s</div>
-            <div class="header">Documento</div>
-            <div class="header">Genero</div>
-            <div class="header">Email</div>
-        </div>
-        <div class="data-container">
-            {teachers.map((teacher, key) => {
-                return (
-                    <div class="row">
-                        <div class="cell">{teacher.first_name}</div>
-                        <div class="cell">{teacher.document}</div>
-                        <div class="cell">{teacher.gender}</div>
-                        <div class="cell">{teacher.email}</div>
+ 
+export default function Table({ data = [], columns = []}) {
+    return (
+        <div className="grid-table">
+            <div className="header-container">
+                {columns.map((column, index) => (
+                    <div className="header" key={index}>{column.header}</div>
+                ))}
+            </div>
+            <div className="data-container">
+                {data.map((item, rowIndex) => (
+                    <div className="row" key={rowIndex}>
+                        {columns.map((column, colIndex) => (
+                            <div className="cell" key={colIndex}>{item[column.field]}</div>
+                        ))}
                     </div>
-                )
-            })}
+                ))}
+            </div>
         </div>
-    </div>
-
     );
 }
--- Front/react_kronos/src/index.js
+++ Front/react_kronos/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/navBar/navBars.jsx';
+import TablaProfes from './components/App-Tabla-Profes/tabla-profes.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar/>
+    <TablaProfes />
   </React.StrictMode>,
   document.getElementById('root')
 );
