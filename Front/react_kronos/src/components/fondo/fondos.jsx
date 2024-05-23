5088130e243a761515c0124c7ebd336930759669
Augusto Guchea <aguchealezama@gmail.com>
2024-05-23 09:59:42 -0300

    hola fiyol

--- Front/react_kronos/src/components/fondo/fondos.jsx
+++ Front/react_kronos/src/components/fondo/fondos.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './fondos.scss';
+import Modal from '../modal/modals';
+import Button from '../button/buttons';
 
 export default function Fondo() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <section>
-            
+            <Button onClick={handleButtonClick} text="Boton genial" life />
+            {isModalOpen && <Modal onClose={handleCloseModal} title="Titulo del Modal" />}
         </section>
     );
 }
--- Front/react_kronos/src/pages/home/Home.js
+++ Front/react_kronos/src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../../components/navBar/navBars';
 import Fondo from '../../components/fondo/fondos';
+import Modal from '../../components/modal/modals';
 
 export default function Home() {
     return (
