aa7e485d892dc734d74bf0c7263ae6fb95716b84
Teo Abril <teo.abril10@gmail.com>
2024-05-28 15:33:02 -0300

    se empezo el model de crear materia

--- Front/react_kronos/src/pages/ver-materias/materias.jsx
+++ Front/react_kronos/src/pages/ver-materias/materias.jsx
@@ -5,13 +5,15 @@ import Table from '../../components/table/tables.jsx';
 import Select from '../../components/select/select.jsx';
 import Buscador from '../../components/Buscador/buscador.jsx';
 import Modal from '../../components/modal/modals.jsx';
+import Input from "../../components/input/inputs.jsx";
 import RangeSlider from '../../components/timerangeslider/timerange.jsx';
 import './materias.scss';
 
 
 export default function Materias() {
     const [teachers, setTeachers] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [materias, setMaterias] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(true);
     
     useEffect(() => {
         fetch('http://127.0.0.1:8000/Kronosapp/subjects/', {
@@ -24,7 +26,7 @@ export default function Materias() {
             return response.json();
         })
         .then(data => {
-            setTeachers(data);
+            setMaterias (data);
         })
         .catch(error => console.error('Error fetching data:', error));
     }, []);
@@ -37,8 +39,32 @@ export default function Materias() {
         { header: 'Color', field: 'color' },
         { header: 'Descripcion', field: 'description' }
     ];
-    
 
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/Kronosapp/teachers/', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"school_id": 1 })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const teacherNames = data.map(teacher => teacher.first_name + ' ' + teacher.last_name);
+            setTeachers(teacherNames);
+          })
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+    const anios = ['1', '2', '3', '4', '5', '6']
+    const cursos = ['A', 'B', 'C']
+    
     const handleButtonClick = () => {
         setIsModalOpen(true);
     };
@@ -56,15 +82,50 @@ export default function Materias() {
         <NavBar />
         <Fondo >
         <div Class="filtros-container">
-            <Select datos={datos} name="Materia" style={{'--largo': `50`}}/>
+            <Select datos={teachers} name="Materia" style={{'--largo': `50`}}/>
             <Select datos={datos} name="General" style={{'--largo': `50`}}/>
             <Buscador />
             </div>
             <div Class="tabla-container">
-            <Table data={teachers} columns={columns} />
+            <Table data={materias} columns={columns} />
         </div>
         </Fondo>
-        {isModalOpen && <Modal onClose={handleCloseModal} title="Agregar materia" />}
+        {isModalOpen && <Modal onClose={handleCloseModal} title="Agregar materia" >
+                <div Class='Contenedor' style={{display: 'flex',flexDirection: 'row', gap: '20px',  alignItems: 'center'}}>
+                    <div>
+                        <h1>Materia</h1>
+                        <Input />
+                    </div>
+                    <div>
+                        <h1>Abreviacion</h1>
+                        <Input />
+                    </div>
+                </div>
+                <div Class='Contenedor' style={{display: 'flex',flexDirection: 'row', gap: '20px',  alignItems: 'center'}}>
+                    <Select datos={teachers} name="Profesor" style={{'--largo': `1000`}}/>
+                    <Select datos={anios} name="años" style={{'--largo': `700`}}/>
+                </div>
+                <div Class='Contenedor' style={{display: 'flex',flexDirection: 'row', gap: '20px',  alignItems: 'center'}}>
+                    <div>
+                        <h1>Horas Semanales</h1>
+                        <Input type="number"/>
+                    </div>
+                    <div>
+                        <h1>Color</h1>
+                        <Input type="color"/>
+                    </div>
+                </div>
+                <div>
+                    <h1>Plan de Estudio</h1>
+                    <Input textArea/>
+                </div>
+                <div>
+                    <h1>Descripción</h1>
+                    <Input textArea/>
+                </div>
+
+
+            </Modal>}
     </React.StrictMode>
     )
 }
