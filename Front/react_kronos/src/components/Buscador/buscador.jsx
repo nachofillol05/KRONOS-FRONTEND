4eb506ecf106c01a4c7add02966ad96a74b4a1a8
nachofillol05 <nachofillol05@gmail.com>
2024-05-28 11:57:57 -0300

    Modificacion del buscador

--- Front/react_kronos/src/components/Buscador/buscador.jsx
+++ Front/react_kronos/src/components/Buscador/buscador.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './buscador.scss';
 
 export default function Buscador() {
-
     return (
-            <div>
-                <input type="search" placeholder="Buscador"/>
-                
-            </div>
-
+        <div className="buscador-container">
+            <input type="search" placeholder="Buscador"/>
+            <span role="img" aria-label="lupa" className="lupa">&#128269;</span>
+        </div>
     )
 }
