2717612e825b72a4b9f1928f2bb9d12acefd6f7b
nachofillol05 <nachofillol05@gmail.com>
2024-06-04 14:15:08 -0300

    se mantiene logeada la sesion

--- Front/react_kronos/src/pages/home/Home.js
+++ Front/react_kronos/src/pages/home/Home.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import Navegacion from '../../layout/navegacion/navegaciones';
+import { useNavigate } from 'react-router-dom';
+import {useState } from 'react';
 
 export default function Home() {
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState([{}]);
+    fetch('http://localhost:8000/Kronosapp/verifyToken/', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "token": localStorage.getItem('token'),
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                console.log('No es valido el token');
+                navigate('/login'); 
+            }
+            if (response.status === 200) {
+                console.log('funciono el token');
+                setUserData(response.data);
+            }
+            else {
+                navigate('/login');
+            }
+        })
+    function logOut(){
+        console.log('logout')
+        localStorage.setItem('token', '');
+    }
+
     return (
         <React.StrictMode>
             <Navegacion />
+            <button onClick={logOut}>holaaa</button>
         </React.StrictMode>
 );
 }
