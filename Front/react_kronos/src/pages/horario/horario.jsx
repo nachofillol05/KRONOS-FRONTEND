24cd20a80e053d284c3eb492f8ac2bf59a586ffd
nachofillol05 <nachofillol05@gmail.com>
2024-09-24 14:35:27 -0300

    Anda el loading y el horario ya se genera automaticamente, te deja aceptar rechazar y eso

--- Front/react_kronos/src/pages/horario/horario.jsx
+++ Front/react_kronos/src/pages/horario/horario.jsx
@@ -1,4 +1,3 @@
-// Horario.jsx
 import React, { Suspense, lazy, useState, useEffect, useCallback, useMemo } from 'react';
 import { FloatButton, Drawer, Button, Segmented, DatePicker, Modal, Spin } from 'antd';
 import { InsertRowAboveOutlined, DownOutlined, UpOutlined, DownloadOutlined, HistoryOutlined, CloseOutlined, AppstoreOutlined, UserSwitchOutlined, EyeOutlined, EditOutlined, FilterOutlined } from '@ant-design/icons';
@@ -10,11 +9,12 @@ const SelectTeacher = lazy(() => import('./selectTeacher.jsx'));
 const Historial = lazy(() => import('./historial.jsx'));
 const Horas = lazy(() => import('./infoHour.jsx'));
 const SelectCourse = lazy(() => import('./selectCourses.jsx'));
+
 const roles = [
     { value: '1', label: 'Profesor 1' },
     { value: '2', label: 'Profesor 2' },
     { value: '3', label: 'Profesor 3' },
-]
+];
 
 const dateFormat = 'DD/MM/YYYY';
 
@@ -22,12 +22,61 @@ function Horario() {
     const [open, setOpen] = useState(false);
     const [drawerContent, setDrawerContent] = useState(null);
     const [drawerTitle, setDrawerTitle] = useState(null);
-    const [subjects, setSubjects] = useState([]);
+    const [subjects, setSubjects] = useState(null);
     const [editar, setEditar] = useState(false);
     const [mostrarAceptar, setMostrarAceptar] = useState(false);
     const [incomplete, setIncomplete] = useState([]);
     const [showModal, setShowModal] = useState(false);
-    const [Loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [tipIndex, setTipIndex] = useState(0);
+    const [cargandoAuto, setCargandoAuto] = useState(false);
+
+    const messages = [
+        "Sabias que Kronos iba a llamarse Monica en un principio...",
+        "Indios trabajando...",
+        "Cargando...",
+        "En serio no salgas porfas"
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTipIndex((prevIndex) => (prevIndex + 1) % messages.length);
+        }, 5000); // Cambia de mensaje cada 5 segundos
+    
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [messages.length]);
+
+    // Obtención de datos del servidor (materias)
+    useEffect(() => {
+        const url = new URL('http://127.0.0.1:8000/api/viewschedule/');
+        fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: `Token ${localStorage.getItem("token")}`,
+                "School-ID": sessionStorage.getItem("actual_school"),
+            },
+        })
+            .then(response => {
+                if (!response.ok) throw new Error("Network response was not ok");
+                return response.json();
+            })
+            .then(data => {
+                setSubjects(data.length ? data : []);
+                setLoading(false); // Aquí cambias a false después de cargar los datos
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setLoading(false); // Asegúrate de también setear loading a false en caso de error
+            });
+    }, []);
+
+    const showDrawer = useCallback((content, title) => {
+        setDrawerTitle(title);
+        setDrawerContent(content);
+        setOpen(true);
+    }, []);
 
     const openModal = () => {
         setShowModal(true);
@@ -45,6 +94,8 @@ function Horario() {
     }, []);
 
     const generarHorario = () => {
+        setCargandoAuto(true);
+        setLoading(true);
         fetch('http://127.0.0.1:8000/api/new_schedule/', {
             method: 'GET',
             headers: {
@@ -55,19 +106,17 @@ function Horario() {
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log(data[0]);
-                console.log(data[1]);
-                setSubjects(data[0]);
-                setIncomplete(data[1]);
-                if (data[1].length > 0){
+                setLoading(false);
+                setMostrarAceptar(true);
+                if (data[1].length > 0) {
+                    setIncomplete(data[1]);
                     openModal();
                 }
+                setSubjects(data[0]);
             });
-        setMostrarAceptar(true);
-
     }
+
     const aceptarHorario = () => {
-        console.log("Aceptar horario");
         fetch('http://127.0.0.1:8000/api/create_schedule/', {
             method: 'POST',
             headers: {
@@ -78,10 +127,8 @@ function Horario() {
         })
             .then((response) => {
                 if (response.ok) {
-                    console.log("Horario aceptado");
                     return response.json();
                 } else {
-                    console.log("Horario no aceptado");
                     throw new Error('Network response was not ok');
                 }
             })
@@ -95,33 +142,11 @@ function Horario() {
         setIncomplete([]);
     }
 
-    // Obtención de datos del servidor (materias)
-    useEffect(() => {
-        const url = new URL('http://127.0.0.1:8000/api/viewschedule/');
-        //if (searchName) url.searchParams.append('date', searchName);
-        //if (teachers) url.searchParams.append('teachers', teachers);
-        //if (courses) url.searchParams.append('courses', courses);
-        fetch(url, {
-            method: "GET",
-            headers: {
-                Authorization: `Token ${localStorage.getItem("token")}`,
-                "School-ID": sessionStorage.getItem("actual_school"),
-            },
-        })
-            .then(response => {
-                if (!response.ok) throw new Error("Network response was not ok");
-                return response.json();
-            })
-            .then(data => setSubjects(data))
-            .catch(error => console.error("Error fetching data:", error));
-            setLoading(false);
-    }, []);
-
-    const showDrawer = useCallback((content, title) => {
-        setDrawerTitle(title);
-        setDrawerContent(content);
-        setOpen(true);
-    }, []);
+    const cancelarHorario = () => {
+        setSubjects([]);
+        setMostrarAceptar(false);
+        setIncomplete([]);
+    }
 
     const onClose = useCallback(() => {
         setOpen(false);
@@ -139,117 +164,126 @@ function Horario() {
 
     return (
         <>
-            <div className="contenedor-filtros contenedor-filtros-horario">
-                {/*const rols = data.map(rol => ({
-                    value: rol.id,
-                    label: rol.name,
-                }));
-                setRoles(rols); */}
-            
-                {sessionStorage.getItem('rol') === "Directivo" && (
-                <Segmented
-                    size='large'
-                    options={memoizedSegmentedOptions}
-                    onChange={(value) => setEditar(value === 'Editar')} // Cambia el estado de 'editar'
-                />)}
-                <FilterDropdownTable options={roles} onChange={change} placeholder={'Profesores: '} />
-                <FilterDropdownTable options={roles} onChange={change} placeholder={'Cursos: '} />
-                <DatePicker
-                    size="large"
-                    placeholder="Fecha"
-                    style={{
-                        width: 200,
-                    }}
-                    format={dateFormat} 
-                    allowClear
-                />
-            </div>
-
-            {/* Pasar el estado editar al calendario */}
-            <CalendarioDirectivo materias={subjects} mibooleano={editar}/>
-            <div>
-                {sessionStorage.getItem('rol') === "Directivo" && subjects.length === 0 && !mostrarAceptar? (
-                    <Button type="primary" onClick={generarHorario}>Generar automáticamente</Button>
-                ) : sessionStorage.getItem('rol') === "Directivo" && mostrarAceptar ? (
-                    <Button type="primary" onClick={aceptarHorario}>Aceptar horario</Button>
-                ) : null}
-            </div>	
-            
-            <FloatButton.Group
-                visibilityHeight={1500}
-                trigger="click"
-                type="primary"
-                closeIcon={<DownOutlined />}
-                icon={<UpOutlined />}
-            >
-                <FloatButton icon={<DownloadOutlined />} tooltip="Descargar tabla" />
-                <FloatButton icon={<UserSwitchOutlined />} type='primary' tooltip="Seleccionar profesores"
-                    onClick={() => showDrawer(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <SelectTeacher />
-                        </Suspense>, 'Seleccione un profesor')}
-                />
-                <FloatButton icon={<HistoryOutlined />} type='primary' tooltip="Historial de cambios"
-                    onClick={() => showDrawer(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Historial />
-                        </Suspense>, 'Historial de cambios')}
-                />
-                <FloatButton icon={<InsertRowAboveOutlined />} type='primary' tooltip="Horas catedra"
-                    onClick={() => showDrawer(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Horas showDrawer={showDrawer} />
-                        </Suspense>, 'Horas catedra')}
-                />
-                <FloatButton icon={<AppstoreOutlined />} type='primary' tooltip='Cursos'
-                    onClick={() => showDrawer(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <SelectCourse showDrawer={showDrawer} />
-                        </Suspense>, 'Cursos')}
-                />
-            </FloatButton.Group>
-            
-            <Drawer
-                destroyOnClose={false}
-                width={600}
-                title={drawerTitle}
-                onClose={onClose}
-                open={open}
-                closeIcon={false}
-                extra={
-                    <Button onClick={onClose} size='large' type='tertiary' icon={<CloseOutlined />} />
+            <Spin 
+                spinning={loading} 
+                tip={
+                    cargandoAuto ? (
+                    <div>
+                        <div>Esto podría demorar unos minutos...</div>
+                        <div>Por favor, no salga de la página</div>
+                        <div>{messages[tipIndex]}</div>
+                    </div>
+                    ) : (
+                    <div>
+                        <div>Esto podría demorar unos segundos...</div>
+                        <div>Cargando la tabla...</div>
+                    </div>
+                    )
                 }
             >
-                <div style={{ width: '100%', height: '100%' }}>
-                    {drawerContent}
+
+                <div className="contenedor-filtros contenedor-filtros-horario">
+                    {sessionStorage.getItem('rol') === "Directivo" && !mostrarAceptar && (
+                        <Segmented
+                            size='large'
+                            options={memoizedSegmentedOptions}
+                            onChange={(value) => setEditar(value === 'Editar')}
+                        />
+                    )}
+                    {!mostrarAceptar ? (
+                        <>
+                            <FilterDropdownTable options={roles} onChange={change} placeholder={'Profesores: '} />
+                            <FilterDropdownTable options={roles} onChange={change} placeholder={'Cursos: '} />
+                            <DatePicker
+                                size="large"
+                                placeholder="Fecha"
+                                style={{ width: 200 }}
+                                format={dateFormat}
+                                allowClear
+                            />
+                        </>
+                    ) : null}
                 </div>
-            </Drawer>
-            
-            {showModal?(
-                <Modal
-                title="Error al generar horario"
-                visible={showModal}
-                onCancel={closeModal}
-                footer={[
-                    <Button key="ok" onClick={closeModal}>
-                        Cerrar
-                    </Button>
-                ]}
-            >
+
+                <CalendarioDirectivo materias={subjects ? subjects : []} mibooleano={editar} />
                 <div>
-                    <h2>Horario incompleto</h2>
-                    <p>El horario no se ha completado, faltan las siguientes materias:</p>
-                    <ul>
-                        {incomplete.map((materia, index) => (
-                            <li key={index}>{materia}</li>
-                        ))}
-                    </ul>
+                    {sessionStorage.getItem('rol') === "Directivo" && !mostrarAceptar && !subjects?.length && !loading ? (
+                        <Button type="primary" onClick={generarHorario}>Generar automáticamente</Button>
+                    ) : sessionStorage.getItem('rol') === "Directivo" && mostrarAceptar ? (
+                        <>
+                            <Button type="primary" onClick={aceptarHorario}>Aceptar horario</Button>
+                            <Button type="primary" onClick={cancelarHorario}>Cancelar horario</Button>
+                        </>
+                    ) : null}
                 </div>
-            </Modal>
-            
-            ): null}
-            </>
+                
+                <FloatButton.Group
+                    visibilityHeight={1500}
+                    trigger="click"
+                    type="primary"
+                    closeIcon={<DownOutlined />}
+                    icon={<UpOutlined />}
+                >
+                    <FloatButton icon={<DownloadOutlined />} tooltip="Descargar tabla" />
+                    <FloatButton icon={<UserSwitchOutlined />} type='primary' tooltip="Seleccionar profesores"
+                        onClick={() => showDrawer(
+                            <Suspense fallback={<Spin />}><SelectTeacher /></Suspense>, 'Seleccione un profesor')}
+                    />
+                    <FloatButton icon={<HistoryOutlined />} type='primary' tooltip="Historial de cambios"
+                        onClick={() => showDrawer(
+                            <Suspense fallback={<Spin />}><Historial /></Suspense>, 'Historial de cambios')}
+                    />
+                    <FloatButton icon={<InsertRowAboveOutlined />} type='primary' tooltip="Horas catedra"
+                        onClick={() => showDrawer(
+                            <Suspense fallback={<Spin />}><Horas showDrawer={showDrawer} /></Suspense>, 'Horas catedra')}
+                    />
+                    <FloatButton icon={<AppstoreOutlined />} type='primary' tooltip='Cursos'
+                        onClick={() => showDrawer(
+                            <Suspense fallback={<Spin />}><SelectCourse showDrawer={showDrawer} /></Suspense>, 'Cursos')}
+                    />
+                </FloatButton.Group>
+                
+                <Drawer
+                    destroyOnClose={false}
+                    width={600}
+                    title={drawerTitle}
+                    onClose={onClose}
+                    open={open}
+                    closeIcon={false}
+                    extra={
+                        <Button onClick={onClose} size='large' type='tertiary' icon={<CloseOutlined />} />
+                    }
+                >
+                    <div style={{ width: '100%', height: '100%' }}>
+                        {drawerContent}
+                    </div>
+                </Drawer>
+                
+                {showModal ? (
+                    <Modal
+                        title="Error al generar horario"
+                        visible={showModal}
+                        onCancel={closeModal}
+                        footer={[
+                            <Button key="ok" onClick={closeModal}>
+                                Cerrar
+                            </Button>
+                        ]}
+                    >
+                        <div>
+                            <h2>Horario incompleto</h2>
+                            <p>El horario no se ha completado, faltan las siguientes materias:</p>
+                            <ul>
+                                {incomplete.map((materia, index) => (
+                                    <li key={index}>{materia}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </Modal>
+                ) : null}
+            </Spin>
+        </>
     );
 }
 
-export default React.memo(Horario);
+export default Horario;
