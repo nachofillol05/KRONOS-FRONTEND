1dc286e81f29c29b4727a2c1082e1a75a3f280a3
nachofillol05 <nachofillol05@gmail.com>
2024-11-16 16:37:58 -0300

    FUncionalidad  muy fachera agregada que si no tiene la disp cargada en especific worker hay un boton que le manda un mail recordandole. Anda peor no el loading y el message

--- Kronos/src/pages/personal/especificWorker.jsx
+++ Kronos/src/pages/personal/especificWorker.jsx
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Button, Flex, Divider, Col, Row, Tooltip, Image, Skeleton,List } from "antd";
+import {
+  Button,
+  Flex,
+  Divider,
+  Col,
+  Row,
+  Tooltip,
+  Image,
+  message,
+  Spin,
+  List,
+} from "antd";
 import {
   RollbackOutlined,
   PlusOutlined,
@@ -7,48 +18,32 @@ import {
 } from "@ant-design/icons";
 import "./personal.scss";
 
-export default function Especificworker({ handleVolverInfo, user, onClose, rolSeleccionado }) {
-  const data = user?.subjects?[...new Set(user.subjects
-    .map(subject => subject.subject_name)
-    .filter(subjectName => subjectName.trim() !== "")
-  )]:[];
-  console.log(data)
+export default function Especificworker({
+  handleVolverInfo,
+  user,
+  onClose,
+  rolSeleccionado,
+}) {
+  const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+  const data = user?.subjects
+    ? [
+        ...new Set(
+          user.subjects
+            .map((subject) => subject.subject_name)
+            .filter((subjectName) => subjectName.trim() !== "")
+        ),
+      ]
+    : [];
+  console.log(data);
   console.log(user);
-  console.log(rolSeleccionado)
+  console.log(rolSeleccionado);
   const [selectedCells, setSelectedCells] = useState([]);
   const [worker, setworker] = useState(user);
   const [modulesData, setModulesData] = useState([]);
-  const [isSkeleton, setIsSkeleton] = useState(true);
 
   const days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
 
-  /*useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/staff/", {
-      method: "GET",
-      headers: {
-        Authorization: "Token " + localStorage.getItem("token"),
-        "School-ID": sessionStorage.getItem("actual_school"),
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const filteredworker = data.find((worker) => worker.id === user.id);
-        if (filteredworker) {
-          setworker(filteredworker);
-          console.log(filteredworker);
-        } else {
-          console.log("worker? not found");
-          setworker(null); // O maneja el caso donde no se encuentra el trabajador
-        }
-        setIsSkeleton(false);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, [user.id]);*/
   useEffect(() => {
     fetch("http://localhost:8000/api/modules/", {
       method: "GET",
@@ -65,17 +60,58 @@ export default function Especificworker({ handleVolverInfo, user, onClose, rolSe
       });
   }, []);
 
+  const EnviarRecordatorio = () => {
+    setLoading(true);
+    const bodyData = {
+      asunto: "Recuerde cargar su disponibilidad horaria",
+      contenido: "Un directivo de la institución le recuerda que aún no ha cargado su disponibilidad horaria, intente hacerlo lo antes posible asi podremos asignarle los horarios de sus materias. Saludos. Link: http://localhost:3000/perfil",//CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMBIASR DESPUES POR LA URL DEL PROYECTO ENSERIO
+      teacher_mail: worker.email, // Cambiamos a un array simple
+    };
+
+
+    fetch("http://127.0.0.1:8000/api/contacting-staff/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData), // Usa bodyData aquí
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        messageApi.success("Mail enviado correctamente");
+        onClose();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false); // Para detener el spinner en caso de error
+      });
+  };
+
+  console.log(loading);
+
   return (
-    <Skeleton loading={false} active>
+  <div>
+      {contextHolder}
       <Flex vertical gap={10}>
         <Flex align="center" gap={30} style={{ width: "70%", height: "50px" }}>
           <label>Foto de perfil:</label>
-          <div style={{ borderRadius: '50%', width:50, height: 50}}>
-                      <img
-                      src={worker.profile_picture? worker.profile_picture : "https://via.placeholder.com/150"}
-                      style={{ objectFit: 'cover', width: '100%', height: '100%', borderRadius: '50%' }}
-                    />
-                    </div>
+          <div style={{ borderRadius: "50%", width: 50, height: 50 }}>
+            <img
+              src={
+                worker.profile_picture
+                  ? worker.profile_picture
+                  : "https://via.placeholder.com/150"
+              }
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+                borderRadius: "50%",
+              }}
+            />
+          </div>
         </Flex>
         <Flex gap={30}>
           <label>
@@ -105,80 +141,100 @@ export default function Especificworker({ handleVolverInfo, user, onClose, rolSe
       </Flex>
       {rolSeleccionado === "Profesores" && (
         <>
-      <Divider orientation="left">Disponiblidad horaria</Divider>
-      <div>
-      <Row className="especificWorkerCasilla">
-  {days.map((day) => {
-    const moduleData = modulesData.filter(
-      (data) => data.day.toLowerCase() === day.toLowerCase()
-    );
+          <Divider orientation="left">Disponiblidad horaria</Divider>
+          {worker?.availability?.length == 0 && (
+            <>
+              Aún no cargó su disponibilidad horaria
+              <Tooltip title="Se enviará un email al usuario para recordarle">
+                <Button
+                  size="small"
+                  onClick={() => EnviarRecordatorio()}
+                  style={{ marginLeft: 8 }}
+                >
+                  Recordar
+                </Button>
+              </Tooltip>
+            </>
+          )}
 
-    return (
-      <Col style={{ flexGrow: 1 }} key={day}>
-        <Row style={{ display: "flex", justifyContent: "center" }}>
-          {day}
-        </Row>
-        <React.Fragment key={day}>
-          {moduleData.map((module) => {
-            console.log(worker.availability  )
-            console.log(module)
-            console.log(day)
-            // Verificar si el módulo está en la disponibilidad del profesor
-            const isAvailable = worker?.availability?.some(
-              (availabilityItem) =>
-                availabilityItem.module === module.id &&
-                availabilityItem.state === "Disponible"
-            );
-            const isOcupied = worker?.availability?.some(
-              (availabilityItem) =>
-                availabilityItem.module === module.id &&
-                availabilityItem.state === "Asignado"
-            );
+          <div>
+            <Row className="especificWorkerCasilla">
+              {days.map((day) => {
+                const moduleData = modulesData.filter(
+                  (data) => data.day.toLowerCase() === day.toLowerCase()
+                );
 
-            return (
-              <Col
-                style={{
-                  width: "95%",
-                  paddingInline: 3,
-                  marginInline: 'auto',
-                  backgroundColor: isAvailable ? "green" : isOcupied? "red" : "transparent", // Pintar de verde si está disponible
-                }}
-                key={`${day}-${module.id}`}
-                className="especificWorkerCasilla"
-              >
-              </Col>
-            );
-          })}
-        </React.Fragment>
-      </Col>
-    );
-  })}
-</Row>
-</div>
-{worker.subjects?.length > 0 && (
-          <>
-            <Divider orientation="left">Materias</Divider>
-                <List
+                return (
+                  <Col style={{ flexGrow: 1 }} key={day}>
+                    <Row style={{ display: "flex", justifyContent: "center" }}>
+                      {day}
+                    </Row>
+                    <React.Fragment key={day}>
+                      {moduleData.map((module) => {
+                        console.log(worker.availability);
+                        console.log(module);
+                        console.log(day);
+                        // Verificar si el módulo está en la disponibilidad del profesor
+                        const isAvailable = worker?.availability?.some(
+                          (availabilityItem) =>
+                            availabilityItem.module === module.id &&
+                            availabilityItem.state === "Disponible"
+                        );
+                        const isOcupied = worker?.availability?.some(
+                          (availabilityItem) =>
+                            availabilityItem.module === module.id &&
+                            availabilityItem.state === "Asignado"
+                        );
+
+                        return (
+                          <Col
+                            style={{
+                              width: "95%",
+                              paddingInline: 3,
+                              marginInline: "auto",
+                              backgroundColor: isAvailable
+                                ? "green"
+                                : isOcupied
+                                ? "red"
+                                : "transparent", // Pintar de verde si está disponible
+                            }}
+                            key={`${day}-${module.id}`}
+                            className="especificWorkerCasilla"
+                          ></Col>
+                        );
+                      })}
+                    </React.Fragment>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
+          <Divider orientation="left">Materias</Divider>
+          {data > 0 ? (
+            <>
+              <List
                 size="small"
                 bordered
                 dataSource={data}
                 renderItem={(item) => <List.Item>{item}</List.Item>}
-            />
-          </>
-            )}
-</>)}
-{worker.years?.length > 0 && rolSeleccionado === "Preceptores" && (
-          <>
-            <Divider orientation="left">Años</Divider>
-                <List
-                size="small"
-                bordered
-                dataSource={worker.years}
-                renderItem={(item) => <List.Item>{item.name}</List.Item>}
-            />
-          </>
-            )}
-
+              />
+            </>
+          ) : (
+            <>No tiene materias asignadas</>
+          )}
+        </>
+      )}
+      {worker.years?.length > 0 && rolSeleccionado === "Preceptores" && (
+        <>
+          <Divider orientation="left">Años</Divider>
+          <List
+            size="small"
+            bordered
+            dataSource={worker.years}
+            renderItem={(item) => <List.Item>{item.name}</List.Item>}
+          />
+        </>
+      )}
 
       <br />
       <Flex justify="flex-end" gap={10}>
@@ -193,6 +249,6 @@ export default function Especificworker({ handleVolverInfo, user, onClose, rolSe
         </Tooltip>
       </Flex>
       <br />
-    </Skeleton>
+  </div>
   );
 }
